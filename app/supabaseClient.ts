14c64c5c0ff81f143a192400dace2de9c996198a
Kazutora <230350198+mrkazutora7@users.noreply.github.com>
2025-09-07 04:43:48 +0530

    Add files via upload

---
+++ app/supabaseClient.ts
@@ -0,0 +1,7 @@
+// apps/web/lib/supabaseClient.ts
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = 'https://ridvgugkeiiwimwbusfs.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJpZHZndWdrZWlpd2ltd2J1c2ZzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTcxODUwNTgsImV4cCI6MjA3Mjc2MTA1OH0.TKPLao0kr8FekzQwhfrzRtWEN-xdozbChMqERryGZPY'
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
