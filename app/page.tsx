2cdefcb07cde223a3c8294c4d8b019ed46b29965
Kazutora <230350198+mrkazutora7@users.noreply.github.com>
2025-09-07 23:11:51 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useEffect, useState } from 'react'
-import { supabase } from '@/lib/supabaseClient.ts'
+import { supabase } from '@/lib/supabaseClient'
 
 type Project = {
   id: string
